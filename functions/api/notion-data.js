f44c79e4664614f61ab30bc8165747668a613ee8
srpepemetro <pepemetro@gmail.com>
2026-04-27 09:59:12 +0200

    tmp: add debug info to api response

--- functions/api/notion-data.js
+++ functions/api/notion-data.js
@@ -73,7 +73,12 @@ export async function onRequestGet(context) {
       };
     }).filter(p => p.proyecto);
 
-    return new Response(JSON.stringify({ transactions, projects }), {
+    const debug = txResults[0] ? {
+      keys: Object.keys(txResults[0].properties),
+      bruto_raw: txResults[0].properties['Bruto cobrado'],
+    } : null;
+
+    return new Response(JSON.stringify({ transactions, projects, debug }), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (err) {
